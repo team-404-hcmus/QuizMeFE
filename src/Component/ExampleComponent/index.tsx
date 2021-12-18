3a3cfad027bad0a210dbd243962f291236ca1514
NguyenVux <rockmanx000@gmail.com>
2021-12-18 15:10:19 +0700

    fix config

--- src/Component/ExampleComponent/index.tsx
+++ src/Component/ExampleComponent/index.tsx
@@ -1,1 +1,8 @@
+import "./test.css";
+function BTS(props:any){
+	let msg = props?.message  ?? "Message not found";
+	msg += "19010923212";
+	return <p className="black">{msg}</p>;
 
+}
+export {BTS}
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {BTS} from "Component/ExampleComponent";
 
+ReactDOM.render(<>
+<BTS message = "HELLO TUAN CUI">
+
+</BTS>
+</>, document.getElementById("root"));
