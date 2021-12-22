e09570220a2b0d8af3e1ad335aacbf46b06b4659
Nguyễn Anh Tuấn <19127614@student.hcmus.edu.vn>
2021-12-22 16:13:55 +0700

    new landing page

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -30,19 +30,23 @@ function LandingPage()
 			{state?<LoginForm></LoginForm>:null}
 			<div id="overlay" onClick={offOverlay}>		
 			</div>
-			<div id="LandingContainer">		
+			<div id="LandingContainer"
+			style={{ 
+				backgroundImage: `url(${process.env.PUBLIC_URL + '/images/background.png'})`,
+				backgroundPosition: 'center',
+				backgroundSize: 'cover',
+				backgroundRepeat: 'no-repeat' }}>	
+					
 				<div className="LgBtnContainer">
 					<button className="LoginButton"onClick={clickLogin}>Login</button>
-					
 				</div>		
 				<form className="middleLP">
 					<div className="Test">
 						<h1> JOIN QUIZME</h1>
 						<h2> Educational Inspiration</h2>
-						<div className="h3container">
-							<h3> The power of interactive is in your device
-							We bring the ease of learning to your student</h3>
-						</div>					
+						<h3> The power of interactive is in your device
+							We bring the ease of learning to your student</h3>				
+						<img className="landingImg" src={process.env.PUBLIC_URL+"/images/middlanding.png"} width="100" alt="test"></img>													
 					</div>			
 				</form>
 			</div>
