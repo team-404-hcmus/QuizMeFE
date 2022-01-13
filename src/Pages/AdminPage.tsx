d4b87c2d26c6d391ed65c7d7e58279a658f8f73b
Nguyễn Anh Tuấn <19127614@student.hcmus.edu.vn>
2022-01-13 20:58:55 +0700

    edit account

--- src/Pages/AdminPage.tsx
+++ src/Pages/AdminPage.tsx
@@ -1,8 +1,8 @@
-import React, { Component, useState } from 'react'
+import React, { Component, Fragment, useState } from 'react'
 import "./AdminPage.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import myData from './data.json';
-import {  faPlus,faMinus,faEdit  } from "@fortawesome/free-solid-svg-icons";
+import {  faPlus,faMinus,faEdit,faCheck,faWindowClose  } from "@fortawesome/free-solid-svg-icons";
 function AdminPage(props:any){
     const [accounts, setAccounts] = useState(myData);
     const [addNewAccount, setAddNewAccount] = useState({
@@ -29,7 +29,6 @@ function AdminPage(props:any){
        const newAccountData:any={...addNewAccount};
        newAccountData[fieldName]=fieldValue;
        setAddNewAccount(newAccountData);
-       console.log(addNewAccount);
     }
     const handleAddAccountSubmit = (event:any) => {
         event.preventDefault();
@@ -65,6 +64,15 @@ function AdminPage(props:any){
     
         setEditedAccount(newAcc);
       };
+      function handleEditAccountChange(event:any){
+        event.preventDefault();
+        const fieldName = event.target.getAttribute('name');
+        const fieldValue =event.target.value;
+ 
+        const newAccountData:any={...editedAccount};
+        newAccountData[fieldName]=fieldValue;
+        setEditedAccount(newAccountData);
+     }
     function StudentTable(){ 
         return(  
             <div className='accountTable'>
@@ -84,25 +92,56 @@ function AdminPage(props:any){
                         </tr>
                     </thead>
                     <tbody>
-                        {accounts.reverse().slice(0,accounts.length).map((val:any, key:any) => {
-                        return (
-                            <tr className='bodyRow'>
-                                <td>{val.username}</td>
-                                <td>{val.fullname}</td>
-                                <td>{val.dob}</td>                     
-                                <td>{val.gender}</td>  
-                                <td>
-                                    <div id="actionBtnContainer" >
-                                        <FontAwesomeIcon className='actionBtn' icon={faEdit}
-                                        onClick={(event)=>handleEditClick(event,val)}></FontAwesomeIcon> 
-                                        <FontAwesomeIcon className='actionBtn' icon={faMinus}
-                                        onClick={()=>handleRemove(val.username)}></FontAwesomeIcon>  
-                                    </div>                                 
-                                </td>                     
-                            </tr>
-                            )    
-                        }
-                        )}
+                        {accounts.slice(0,accounts.length).map((val:any, key:any) => (
+                        <Fragment>
+                            {editAccounttId===val.username ?
+                                (                          
+                                    <tr className='bodyRow'>
+                                        <td> 
+                                            <input id="accColumn" type="text" name="username" placeholder='Input School ID' required
+                                            onChange={handleAddAccountChange}/>
+                                        </td>
+                                        <td> 
+                                            <input id="accColumn" type="text" name="fullname" placeholder='Input Full Name' required
+                                            onChange={handleAddAccountChange}/>
+                                        </td>
+                                        <td> 
+                                            <input id="accColumn" type="text" name="dob" placeholder='Input DoB' required
+                                            onChange={handleAddAccountChange}/>
+                                        </td>
+                                        <td> 
+                                            <input id="accColumn" type="text" name="gender" placeholder='Input Gender' required
+                                            onChange={handleAddAccountChange}/>
+                                        </td>
+                                        <td>
+                                            <div id="actionBtnContainer" >
+                                                <FontAwesomeIcon className='actionBtn' icon={faCheck}
+                                                ></FontAwesomeIcon> 
+                                                <FontAwesomeIcon className='actionBtn' icon={faWindowClose}
+                                                ></FontAwesomeIcon>  
+                                            </div>                                 
+                                        </td>                     
+                                    </tr>
+                                ) :
+                                (
+                                    <tr className='bodyRow'>
+                                        <td>{val.username}</td>
+                                        <td>{val.fullname}</td>
+                                        <td>{val.dob}</td>                     
+                                        <td>{val.gender}</td>  
+                                        <td>
+                                            <div id="actionBtnContainer" >
+                                                <FontAwesomeIcon className='actionBtn' icon={faEdit}
+                                                onClick={(event)=>handleEditClick(event,val)}></FontAwesomeIcon> 
+                                                <FontAwesomeIcon className='actionBtn' icon={faMinus}
+                                                onClick={()=>handleRemove(val.username)}></FontAwesomeIcon>  
+                                            </div>                                 
+                                        </td>                     
+                                    </tr>)   
+                                }
+                        </Fragment>
+                        )
+                    )}
                     </tbody>
                     <thead>
                         <tr className="tableName">                     
